73185a16323da59957bac8f796897455223eb5c4
Matt Mancuso <73910562+MancusoM@users.noreply.github.com>
2022-03-15 21:58:00 -0400

    Update Kimbrel.R

--- Kimbrel.R
+++ Kimbrel.R
@@ -6,7 +6,7 @@ library(ggplot2)
 year_data <- `savant_data.(81)`
 year_data$game_date <- as.Date.factor(year_data$game_date)
 print(year_data$game_date)
-#Subsetting the date to before & after the trade deadline
+#Subsetting the date to before & after the trade deadline for potential future use 
 after <- subset(year_data, game_date >= "2021-07-31")
 before <-subset(year_data, game_date <= '2021-07-30')
 #Printing of Plots
